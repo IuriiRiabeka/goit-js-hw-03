ce0299d974cac32e10b672f5d36c286a39176796
IuriiRiabeka <sam2fly7@gmail.com>
2024-01-31 07:31:29 +0200

    Update task3.js
    simpler and bigger code

--- task3.js
+++ task3.js
@@ -1,8 +1,13 @@
 function filterArray(numbers, value) {
-  return numbers.filter(function (number) {
-    return number > value;
-  });
-}
+  let newArray = [];
+  for (const number of numbers) {
+    if (number > value) { newArray.push(number) }
+    else { newArray.push() }
+
+  }
+  return (newArray);
+};
+
 
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
